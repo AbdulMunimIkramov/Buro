dea4586dd21fb6229a0c53f782abd9341163793e
mega-mozg <abdulmunimikramov01@gmail.com>
2024-12-08 02:40:00 +0600

    add favourite

--- src/pages/CARDS/Cards.jsx
+++ src/pages/CARDS/Cards.jsx
@@ -13,6 +13,7 @@ export const Cards = () => {
                 img="https://ae04.alicdn.com/kf/A7a4cb07f4a464302b1b872df9dde5fc4n.jpeg"
                 category="Вещь"
                 description="нашёл сумку в Районе Гума, внутри были перчатки футболка нашёл сумку в гу У ДАУРА БОЛЬШОЙ ЧЛЕН В ЖОПЕ"
+                favourite={true}
             />
             <Card
                 img="https://ae04.alicdn.com/kf/A7a4cb07f4a464302b1b872df9dde5fc4n.jpeg"
